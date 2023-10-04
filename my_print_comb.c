0f38cd2096a146251774e94e8cab4eaed206ebda
Leticia <leticia.fraga@epitech.eu>
2023-10-04 17:09:05 +0200

    fix style

--- my_print_comb.c
+++ my_print_comb.c
@@ -7,23 +7,42 @@
 */
 #include "my_putchar.h"
 
-int my_print_comb(void)
+int conditional_print(int i, int j, int k)
+{
+    if (i != j && k != j && k != i){
+        my_putchar(i);
+        my_putchar(j);
+        my_putchar(k);
+        if (i == 55 && j == 56 && k == 57) {
+            return 0;
+        }
+        my_putchar(',');
+        my_putchar(' ');
+    }
+    return 1;
+}
+
+int validate(int i , int j)
+{
+    for (int k = j + 1; k < 58; k++) {
+        if (conditional_print(i, j, k) == 0) {
+            break;
+        }
+    }
+}
+
+int iterate2(void)
 {
     for (int i = 48; i < 58; i++) {
         for (int j = i + 1; j < 58; j++) {
-            for (int k = j + 1; k < 58; k++) {
-                if (i != j && k != j && k != i){
-                    my_putchar(i);
-                    my_putchar(j);
-                    my_putchar(k);
-                    if (i == 55 && j == 56 && k == 57) {
-                        break;
-                    }
-                    my_putchar(',');
-                    my_putchar(' ');
-                }
-            }
-        }       
+            validate(i, j);
+        }
     }
+    return 0;
+}
+
+int my_print_comb(void)
+{
+    iterate2();
     return 0;
 }
