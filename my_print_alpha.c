7a48c6dc2968d3959a2a2cb92dbc4be23ed6a885
Leticia <leticia.fraga@epitech.eu>
2023-10-04 10:57:58 +0200

    task01

---
+++ my_print_alpha.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_PRINT_APLHA
+** File description:
+** Function that, beginning with a, displays the lowercase alphabet in ascending order, on a single line
+*/
+
+int my_print_alpha ( void )
+{
+    int i;
+    for (i = 97; i <= 122; i++)
+    {
+        my_putchar(i);
+    }
+}
