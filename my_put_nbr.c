e5c89cf4674883f09f90518681f64df275ee1d3a
Leticia <leticia.fraga@epitech.eu>
2023-10-04 18:36:05 +0200

    task07

---
+++ my_put_nbr.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_PUT_NBR
+** File description:
+** Function that displays the number given as a parameter
+*/
+int get_next(int nb)
+{
+    int next = nb % 10;
+
+    nb = nb / 10;
+    if (nb != 0) {
+        get_next(nb);
+    }
+    my_putchar(next + '0');
+}
+
+int my_put_nbr(int nb)
+{
+    if (nb < 0) {
+        my_putchar('-');
+        nb = nb * -1;
+    }
+    get_next(nb);
+    return 0;
+}
