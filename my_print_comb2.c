69a29360d192e50c4abb194f6cb0daba7f7477e1
Leticia <leticia.fraga@epitech.eu>
2023-10-05 02:31:36 -0500

    fix style

--- my_print_comb2.c
+++ my_print_comb2.c
@@ -7,35 +7,41 @@
 */
 #include "my_putchar.h"
 
-int cmp_numbers(int i, int j, int k, int m)
+int print_comb1(int i, int j, int k, int m)
 {
-    if (i == k && j == m)
+    my_putchar(i);
+    my_putchar(j);
+    my_putchar(' ');
+    my_putchar(k);
+    my_putchar(m);
+    if (i == '9' && j == '8') {
         return 0;
+    }
+    my_putchar(',');
+    my_putchar(' ');
     return 1;
 }
 
 int my_print_comb2(void)
 {
-    for (int i = '0'; i < ':'; i++) {
-        for (int j = '0'; j < ':'; j++) {
-            for (int k = i; k < ':'; k++) {
-                for (int m = '0'; m < ':'; m++) {
-                    if (k == i && m <= j) {
-                        m = j + 1;
-                    }
-                    my_putchar(i);
-                    my_putchar(j);
-                    my_putchar(' ');
-                    my_putchar(k);
-                    my_putchar(m);
-                    if (i == '9' && j == '8') {
-                        return 0;
-                    }
-                    my_putchar(',');
-                    my_putchar(' ');                    
-                }
-            }
-        }       
-    }
+    int i = '0';
+    int j = '0';
+    int k = '0';
+    int m = '0';
+    int it1 = 0;
+    int it2 = 1;
+
+    do {
+        i = (it1 / 10) + '0';
+        j = (it1 % 10) + '0';
+        it2 = it1 + 1;
+        do {
+            k = (it2 / 10) + '0';
+            m = (it2 % 10) + '0';
+            print_comb1(i, j, k, m);
+            it2++;
+        } while (it2 < 100);
+        it1++;
+    } while (it1 < 99);
     return 0;
 }
