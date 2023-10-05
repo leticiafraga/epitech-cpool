c2ac30547f09daca19c96f4256e7e4731b8b92df
Leticia <leticia.fraga@epitech.eu>
2023-10-05 08:17:19 -0500

    task 6

---
+++ my_sort_int_array.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_SORT_INT_ARRAY
+** File description:
+** function that sorts an integer array in ascending order,
+** given a pointer to the first element of the array and its size.
+*/
+void my_sort_int_array(int *array, int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j ++) {
+            if (array[j] > array[j + 1]) {
+                my_swap(&array[j], &array[j + 1]);
+            }
+        }
+    }
+}
+
---
+++ tests/tests_my_sort_int_array.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int arr[] = {2, 1, 3, 5, 4};
+    my_sort_int_array(arr, 5);
+    my_putchar(arr[0] + '0');
+    my_putchar(arr[1] + '0');
+    my_putchar(arr[2] + '0');
+    my_putchar(arr[3] + '0');
+    my_putchar(arr[4] + '0');
+
+    return 0;
+}
