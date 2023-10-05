67d01af26b1a094282d4687f6b480dddf21e0a73
Leticia <leticia.fraga@epitech.eu>
2023-10-05 09:16:00 -0500

    fix some style errors

--- my_getnbr.c
+++ my_getnbr.c
@@ -4,6 +4,27 @@
 ** File description:
 ** Returns a number, sent to the function as a string
 */
+int getdigits(char const *str, char *nbr) {
+    int i = 0;
+    char c;
+    int len = 0;
+    int neg = 0;
+
+    do {
+        c = str[i];
+        if (c >= '0' && c <= '9'|| (c == '-' && neg == 0)) {
+            nbr[len] = c;
+            len += 1;
+            if (c == '-') {
+		neg = 1;
+            }
+	}
+        i ++;
+    } while (c != '\0');
+    nbr[len] = '\0';
+    return len;
+}
+
 int my_getnbr(char const *str)
 {
     int i = 0;
@@ -12,18 +33,8 @@ int my_getnbr(char const *str)
     char nbr[10];
     long int result = 0;
     int neg = 0;
-    
-    do {
-        c = str[i];
-        if (c >= '0' && c <= '9'|| (c == '-' && neg == 0)) {
-            nbr[len] = c;
-            len += 1;
-            if (c == '-') {
-                neg = 1;
-            }
-        }
-        i ++;
-    } while (c != '\0');
+
+    len = getdigits(str, nbr);  
     for (i = 0; i < len; i++) {
         result = result * 10;
         result += (nbr[i] - '0');
