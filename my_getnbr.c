da9e6ed05650b64cad6fec9a0b09fea911539b4d
Leticia <leticia.fraga@epitech.eu>
2023-10-05 08:03:14 -0500

    initial task 5

---
+++ my_getnbr.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_GETNBR
+** File description:
+** Returns a number, sent to the function as a string
+*/
+int my_getnbr(char const *str)
+{
+    int i = 0;
+    char c;
+    int len = 0;
+    char nbr[10];
+    long int result = 0;
+    int neg = 0;
+    
+    do {
+        c = str[i];
+        if (c >= '0' && c <= '9'|| (c == '-' && neg == 0)) {
+            nbr[len] = c;
+            len += 1;
+            if (c == '-') {
+                neg = 1;
+            }
+        }
+        i ++;
+    } while (c != '\0');
+    for (i = 0; i < len; i++) {
+        result = result * 10;
+        result += (nbr[i] - '0');
+    }
+    if (result > 2147483647 || result < -2147483647) {
+        return 0;
+    }
+    return result;
+}
