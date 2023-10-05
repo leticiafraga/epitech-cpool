d46422d25bb4e17ea47ac43c0ae4cc041cad1de6
Leticia <leticia.fraga@epitech.eu>
2023-10-05 11:36:05 -0500

    initial star

---
+++ star.c
@@ -0,0 +1,67 @@
+void print_spaces(int n)
+{
+    for (int i = 0; i < n; i++) {
+        my_putchar(' ');
+    }
+}
+
+void print_ast(int n)
+{
+    for (int i = 0; i < n; i++) {
+        my_putchar('*');
+    }
+}
+
+
+void line1(unsigned int size)
+{
+    print_spaces(size*2);
+    print_spaces(size - 1);
+    my_putchar('*');
+    my_putchar('\n');
+}
+
+void top(unsigned int size)
+{
+    int spaces = size*3 - 2;
+    for (int i = 1; i < size; i++) {
+        print_spaces(spaces);
+        my_putchar('*');
+        print_spaces(i*2 - 1);
+        my_putchar('*');    
+        my_putchar('\n');
+        spaces -= 1;
+    }
+}
+
+void bottom(unsigned int size)
+{
+    int spaces = size*2;
+    for (int i = size - 1; i > 0; i--) {
+        print_spaces(spaces);
+        my_putchar('*');
+        print_spaces(i*2 - 1);
+	my_putchar('*');
+	my_putchar('\n');
+        spaces += 1;
+    }
+}
+
+
+void border(unsigned int size)
+{
+    print_ast(size*2 + 1);
+    print_spaces(size*2 - 3);
+    print_ast(size*2 + 1);
+    my_putchar('\n');
+}
+
+void star (unsigned int size)
+{
+    line1(size);
+    top(size);
+    border(size);
+    border(size);
+    bottom(size);
+    line1(size);
+}
---
+++ tests/tests_star.c
@@ -0,0 +1,9 @@
+int main()
+{
+    star(1);
+    star(2);
+    star(3);
+    star(4);
+    star(5);
+    return 0;
+}
