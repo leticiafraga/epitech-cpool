11492de4d096fd3a937e158fb3cac523ac5c0ed3
Leticia <leticia.fraga@epitech.eu>
2023-10-06 03:57:48 -0500

    task6

---
+++ tests/tests_my_is_prime.c
@@ -0,0 +1,14 @@
+int main()
+{
+    my_put_nbr(my_is_prime(5));
+    my_putchar('\n');
+    my_put_nbr(my_is_prime(1));
+    my_putchar('\n');
+    my_put_nbr(my_is_prime(10));
+    my_putchar('\n');
+    my_put_nbr(my_is_prime(0));
+    my_putchar('\n');
+    my_put_nbr(my_is_prime(-4));
+    my_putchar('\n');
+    return 0;
+}
