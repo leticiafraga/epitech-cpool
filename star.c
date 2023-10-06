762985d0bd0517e35c6a33d07d2600c1b3039365
Leticia <leticia.fraga@epitech.eu>
2023-10-06 12:11:13 -0500

    fix style

--- star.c
+++ star.c
@@ -1,3 +1,9 @@
+/*
+** EPITECH PROJECT, 2023
+** STAR
+** File description:
+** Function that displays a star, based on its given size.
+*/
 void print_spaces(int n)
 {
     for (int i = 0; i < n; i++) {
@@ -12,8 +18,7 @@ void print_ast(int n)
     }
 }
 
-
-void line1(unsigned int size)
+static void line1(unsigned int size)
 {
     print_spaces(size * 2);
     print_spaces(size - 1);
@@ -24,46 +29,46 @@ void line1(unsigned int size)
     my_putchar('\n');
 }
 
-void top(unsigned int size)
+static void top(unsigned int size)
 {
     int spaces = (size * 3) - 2;
 
     for (int i = 1; i < size; i++) {
         print_spaces(spaces);
         my_putchar('*');
-        print_spaces(i*2 - 1);
-        my_putchar('*');    
+        print_spaces(i * 2 - 1);
+        my_putchar('*');
         my_putchar('\n');
         spaces -= 1;
     }
 }
 
-void bottom(unsigned int size)
+static void bottom(unsigned int size)
 {
     int spaces = size * 2;
 
     for (int i = size - 1; i > 0; i--) {
         print_spaces(spaces);
         my_putchar('*');
-        print_spaces(i*2 - 1);
-	my_putchar('*');
-	my_putchar('\n');
+        print_spaces(i * 2 - 1);
+        my_putchar('*');
+        my_putchar('\n');
         spaces += 1;
     }
 }
 
-void border(unsigned int size)
+static void border(unsigned int size)
 {
-    print_ast(size*2 + 1);
-    print_spaces(size*2 - 3);
+    print_ast(size * 2 + 1);
+    print_spaces(size * 2 - 3);
     if (size == 1) {
         print_spaces(1);
     }
-    print_ast(size*2 + 1);
+    print_ast(size * 2 + 1);
     my_putchar('\n');
 }
 
-void upmid(unsigned int size)
+static void upmid(unsigned int size)
 {
     for (int i = 0; i < size - 1; i++) {
         print_spaces(i + 1);
@@ -74,7 +79,7 @@ void upmid(unsigned int size)
     }
 }
 
-void mid(unsigned int size)
+static void mid(unsigned int size)
 {
     int mid_spaces = (size * 2) + (size - 2) * 2 + 1;
 
@@ -88,28 +93,28 @@ void mid(unsigned int size)
     my_putchar('\n');
 }
 
-
-void lomid(unsigned int size)
+static void lomid(unsigned int size)
 {
     for (int i = size - 2; i >= 0; i--) {
         print_spaces(i + 1);
         print_ast(1);
         print_spaces((size * 6) - 5 - (2 * i));
         print_ast(1);
-	my_putchar('\n');
+        my_putchar('\n');
     }
 }
 
-
 void star (unsigned int size)
 {
-    line1(size);
-    top(size);
-    border(size);
-    upmid(size);
-    mid(size);
-    lomid(size);
-    border(size);
-    bottom(size);
-    line1(size);
+    if (size > 0) {
+        line1(size);
+        top(size);
+        border(size);
+        upmid(size);
+        mid(size);
+        lomid(size);
+        border(size);
+        bottom(size);
+        line1(size);
+    }
 }
