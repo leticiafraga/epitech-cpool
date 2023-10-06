2bc98073f32dbd12d141092b4c760079cb2c1d3a
Leticia <leticia.fraga@epitech.eu>
2023-10-06 08:28:04 -0500

    fix task3

--- my_strlen.c
+++ my_strlen.c
@@ -5,31 +5,6 @@
 ** Function that counts and returns the number of characters found
 ** in the string passed as parameter.
 */
-
-long int get_next(long int nb)
-{
-    long int next = nb % 10;
-
-    nb = nb / 10;
-    if (nb != 0) {
-        get_next(nb);
-    }
-    my_putchar(next + '0');
-    return 0;
-}
-
-int my_put_nbr(int nb)
-{
-    long int lg = nb;
-
-    if (lg < 0) {
-        my_putchar('-');
-        lg = lg * -1;
-    }
-    get_next(lg);
-    return 0;
-}
-
 int my_strlen(char const *str)
 {
     int i = 0;
@@ -41,6 +16,5 @@ int my_strlen(char const *str)
             i++;
         }
     } while (c != '\0');
-    my_put_nbr(i);
-    return 0;
+    return i;
 }
