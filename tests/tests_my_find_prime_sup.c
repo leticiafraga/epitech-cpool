6632389f0b29c3e18bafb54389040b3b9231f793
Leticia <leticia.fraga@epitech.eu>
2023-10-06 04:07:23 -0500

    task7

---
+++ tests/tests_my_find_prime_sup.c
@@ -0,0 +1,14 @@
+int main()
+{
+    my_put_nbr(my_find_prime_sup(5));
+    my_putchar('\n');
+    my_put_nbr(my_find_prime_sup(1));
+    my_putchar('\n');
+    my_put_nbr(my_find_prime_sup(10));
+    my_putchar('\n');
+    my_put_nbr(my_find_prime_sup(0));
+    my_putchar('\n');
+    my_put_nbr(my_find_prime_sup(-4));
+    my_putchar('\n');
+    return 0;
+}
