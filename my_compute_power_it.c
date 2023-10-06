49387675cef84c735c3ba8117547c070cc8a5cbd
Leticia <leticia.fraga@epitech.eu>
2023-10-06 03:16:42 -0500

    task3

---
+++ my_compute_power_it.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_COMPUTE_POWER_IT
+** File description:
+** Iterative function that returns the first argument raised to
+** the power p, where p is the second argument
+*/
+int my_compute_power_it(int nb, int p)
+{
+    int res = nb;
+
+    if (nb == 0) {
+        return 1;
+    }
+    if (p == 0) {
+        return 0;
+    }
+    for (int i = 1; i < p; i++) {
+        res *= nb;
+    }
+    return res;
+}
