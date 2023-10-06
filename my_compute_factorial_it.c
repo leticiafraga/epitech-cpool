bbaaf7aa874d2d7e337179122ea617d4824a5678
Leticia <leticia.fraga@epitech.eu>
2023-10-06 12:33:48 -0500

    fix task1 error

--- my_compute_factorial_it.c
+++ my_compute_factorial_it.c
@@ -9,7 +9,7 @@ int my_compute_factorial_it(int nb)
 {
     int res = 1;
 
-    if (nb < 0) {
+    if (nb < 0 || nb >= 13) {
         return 0;
     }
     for (int i = 1; i <= nb; i ++) {
--- tests/tests_my_compute_factorial_it.c
+++ tests/tests_my_compute_factorial_it.c
@@ -8,5 +8,9 @@ int main()
     my_putchar('\n');
     my_put_nbr(my_compute_factorial_it(0));
     my_putchar('\n');
+    my_put_nbr(my_compute_factorial_it(12));
+    my_putchar('\n');
+    my_put_nbr(my_compute_factorial_it(13));
+    my_putchar('\n');
     return 0;
 }
