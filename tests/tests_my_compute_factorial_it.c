16976ea2954b1dffcd5c86ce4fb8841b415b14c4
Leticia <leticia.fraga@epitech.eu>
2023-10-06 02:28:46 -0500

    task01

---
+++ tests/tests_my_compute_factorial_it.c
@@ -0,0 +1,12 @@
+int main()
+{
+    my_put_nbr(my_compute_factorial_it(5));
+    my_putchar('\n');
+    my_put_nbr(my_compute_factorial_it(1));
+    my_putchar('\n');
+    my_put_nbr(my_compute_factorial_it(-2));
+    my_putchar('\n');
+    my_put_nbr(my_compute_factorial_it(0));
+    my_putchar('\n');
+    return 0;
+}
