01d53235a21529d7d40ce64453aba119339e7539
Leticia <leticia.fraga@epitech.eu>
2023-10-07 12:26:49 +0200

    initial rush generic

---
+++ rush-generic/tests/tests_rush-generic.c
@@ -0,0 +1,24 @@
+struct square_parts {
+    char corner_1;
+    char corner_2;
+    char corner_3;
+    char corner_4;
+    char body;
+};
+
+int main()
+{
+    struct square_parts square;
+    square.body = 'L';
+    rush_generic(0, 0, &square);
+    rush_generic(0, 1, &square);
+    rush_generic(1, 0, &square);
+    rush_generic(1, 1, &square);
+    rush_generic(1, 5, &square);
+    rush_generic(5, 1, &square);
+    rush_generic(2, 3, &square);
+    rush_generic(3, 4, &square);
+    rush_generic(6, 5, &square);
+    rush_generic(6, -1, &square);
+    rush_generic(-2, 5, &square);
+}
