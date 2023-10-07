c542e608a3ac7a9c4c6cfd9bd36c6d303d4ba6e7
Leticia <leticia.fraga@epitech.eu>
2023-10-07 10:38:51 +0200

    rush-1-1

--- rush-1-1/rush.c
+++ rush-1-1/rush.c
@@ -1,4 +1,11 @@
-void horizontal(int x)
+void print_spaces(int n)
+{
+    for (int i = 0; i < n; i++) {
+        my_putchar(' ');
+    }
+}
+
+void horizontal(int x, int y)
 {
     if (x > 1)
         my_putchar('o');
@@ -9,7 +16,25 @@ void horizontal(int x)
     my_putchar('\n');
 }
 
+void vertical(int x, int y)
+{
+    int body = y - 2;
+    for (int i = 0; i < body; i++) {
+        my_putchar('|');
+        if (x > 1) {
+            print_spaces(x - 2);
+            my_putchar('|');
+        }
+        my_putchar('\n');
+    }
+}
+
 void rush(int x, int y)
 {
-    horizontal(x);
+    if (x == 0 || y == 0)
+        return;
+    horizontal(x, y);
+    vertical(x, y);
+    if (y > 1)
+        horizontal(x, y);
 }
--- rush-1-1/tests/tests_rush-1-1.c
+++ rush-1-1/tests/tests_rush-1-1.c
@@ -1,7 +1,12 @@
 int main()
 {
-    rush(0);
-    rush(1);
-    rush(2);
-    rush(3);
+    rush(0, 0);
+    rush(0, 1);
+    rush(1, 0);
+    rush(1, 1);
+    rush(1, 5);
+    rush(5, 1);
+    rush(2, 3);
+    rush(3, 4);
+    rush(6, 5);
 }
