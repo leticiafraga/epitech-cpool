69a696cbc80a255e9f51c6a5c54ef83f3b879618
Leticia <leticia.fraga@epitech.eu>
2023-10-08 15:40:13 -0500

    more tests

--- tests/tests_my_compute_square_root.c
+++ tests/tests_my_compute_square_root.c
@@ -1,10 +1,14 @@
 int main()
 {
-    my_put_nbr(my_compute_square_root(36));
+    my_put_nbr(my_compute_square_root(-1));
+    my_putchar('\n');
+    my_put_nbr(my_compute_square_root(0));
     my_putchar('\n');
     my_put_nbr(my_compute_square_root(1));
     my_putchar('\n');
     my_put_nbr(my_compute_square_root(-2));
+    my_putchar('\n');
+    my_put_nbr(my_compute_square_root(36));
     my_putchar('\n');
     my_put_nbr(my_compute_square_root(35));
     my_putchar('\n');
