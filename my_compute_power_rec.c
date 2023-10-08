0243787f60aedb1ced9831fb33854d857d238058
Leticia <leticia.fraga@epitech.eu>
2023-10-08 15:38:06 -0500

    fix task 3

--- my_compute_power_rec.c
+++ my_compute_power_rec.c
@@ -7,11 +7,11 @@
 */
 int my_compute_power_rec(int nb, int p)
 {
-    if (p < 0 || nb == 0) {
-        return 0;
-    }
     if (p == 0) {
         return 1;
+    }
+    if (p < 0 || nb == 0) {
+        return 0;
     }
     return nb * my_compute_power_rec(nb, p - 1);
 }
--- tests/tests_my_compute_power_rec.c
+++ tests/tests_my_compute_power_rec.c
@@ -2,11 +2,18 @@ int main()
 {
     my_put_nbr(my_compute_power_rec(5, 3));
     my_putchar('\n');
-    my_put_nbr(my_compute_power_rec(1, 0));
+    my_put_nbr(my_compute_power_rec(4, 0));
+    my_putchar('\n');
+    my_put_nbr(my_compute_power_rec(1, 3));
     my_putchar('\n');
     my_put_nbr(my_compute_power_rec(-2, 3));
     my_putchar('\n');
     my_put_nbr(my_compute_power_rec(0, -1));
     my_putchar('\n');
+    my_put_nbr(my_compute_power_rec(0, 0));
+    my_putchar('\n');
+    my_put_nbr(my_compute_power_rec(0, 1));
+    my_putchar('\n');
+
     return 0;
 }
