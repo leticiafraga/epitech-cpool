c65fe480e392bb998d8a5a469801674247930f80
Leticia <leticia.fraga@epitech.eu>
2023-10-10 11:45:40 +0200

    task1

---
+++ lib/my/my_strcat.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** my_strcat
+** File description:
+** my_showstr
+*/
+
+char *my_strcat(char *dest, char const *src)
+{
+    return 0;
+}
---
+++ lib/my/my_strncat.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** my_strcat
+** File description:
+** my_showstr
+*/
+
+char *my_strncat(char *dest, char const *src, int nb)
+{
+    return 0;
+}
