fbf1eabbea64d5a3feae112d21ae45db117efdde
Leticia <leticia.fraga@epitech.eu>
2023-10-10 14:14:39 +0200

    task5

--- task05/my_rev_params.c
+++ task05/my_rev_params.c
@@ -1,13 +1,14 @@
 /*
 ** EPITECH PROJECT, 2023
-** MY_PRINT_PARAMS
+** MY_REV_PARAMS
 ** File description:
-** Program that displays its arguments (received on the command line).
+** Program that displays all the arguments received on the
+** command line in reverse order
 */
 
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < argc; i++) {
+    for (int i = argc - 1; i >= 0; i--) {
         my_putstr(argv[i]);
         my_putchar('\n');
     }
