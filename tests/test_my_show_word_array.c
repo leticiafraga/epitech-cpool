be4dbfe8758c3e40c052916e54de709c42ebdd12
Leticia <leticia.fraga@epitech.eu>
2023-10-11 10:59:27 +0200

    task3

---
+++ tests/test_my_show_word_array.c
@@ -0,0 +1,7 @@
+int main(int ac, char **av)
+{
+    char * test_word_array [] = { " The ", " Answer ", "to", "the",
+        "Great" , " Question..." , "Of", " Life,", "the", "Universe", "and", 0};
+    my_show_word_array(test_word_array);
+    return 0;
+}
