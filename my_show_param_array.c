8decfc6e4fa638f180e309c29cedaeb74583b6c3
Leticia <leticia.fraga@epitech.eu>
2023-10-12 15:25:06 +0200

    fix order

--- my_show_param_array.c
+++ my_show_param_array.c
@@ -10,10 +10,10 @@ int my_show_param_array(struct info_param *const par)
     int i = 0;
 
     while (par[i].str != 0) {
-        my_put_nbr(par[i].length);
-        my_putchar('\n');
         my_putstr(par[i].str);
         my_putchar('\n');
+        my_put_nbr(par[i].length);
+        my_putchar('\n');  
         my_show_word_array(par[i].word_array);
         i++;
     }
