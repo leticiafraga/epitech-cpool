4659ffc28c0ee026fea672906f561b62ac9a9297
Leticia <leticia.fraga@epitech.eu>
2023-10-14 11:17:43 +0200

    task2

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ int count_letters(char *str, char letter)
     int i;
     int cnt;
 
-    my_strupcase(str);
     if (is_lowcase(letter) == 1) {
         letter -= 32;
     }
@@ -27,17 +26,28 @@ int count_letters(char *str, char letter)
     return (cnt);
 }
 
+void put_info(char c, int cnt)
+{
+    my_putchar(c);
+    my_putchar(':');
+    my_put_nbr(cnt);
+    my_putchar('\n');
+}
+
 int main(int argc, char **argv)
 {
     int cnt;
 
-    if (argc < 3 || is_letter(argv[2][0] == 0)) {
+    if (argc < 3) {
         return (84);
     }
-    cnt = count_letters(argv[1], argv[2][0]);
-    my_putchar(argv[2][0]);
-    my_putchar(':');
-    my_put_nbr(cnt);
-    my_putchar('\n');
+    my_strupcase(argv[1]);
+    for (int i = 2; i < argc; i++) {
+        if (is_letter(argv[2][0] == 0)) {
+            return (84);
+        }
+        cnt = count_letters(argv[1], argv[i][0]);
+        put_info(argv[i][0], cnt);
+    }
     return (0);
 }
