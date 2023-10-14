8d964b2c9b5994b4849c92f1b6980700dc83289f
Leticia <leticia.fraga@epitech.eu>
2023-10-14 16:28:51 +0200

    more than 1 char op

--- my_advanced_do_op/main.c
+++ my_advanced_do_op/main.c
@@ -16,12 +16,12 @@ static void put_err(char *str)
     write(2, str, len);
 }
 
-static int validate_op(char c, int n1, int n2)
+static int validate_op(char *c, int n1, int n2)
 {
     int i = 0;
 
     while (OPERATORS_FUNCS[i].symbol[0] != '\0') {
-        if (c == OPERATORS_FUNCS[i].symbol[0]) {
+        if (my_strstr(c, OPERATORS_FUNCS[i].symbol)) {
             return i;
         }
         i++;
@@ -40,7 +40,6 @@ int main(int ac, char **av)
 {
     int n1;
     int n2;
-    char *op;
     int len;
     int found = -1;
 
@@ -48,10 +47,9 @@ int main(int ac, char **av)
         return 84;
     n1 = my_getnbr(av[1]);
     n2 = my_getnbr(av[3]);
-    op = av[2];
-    len = my_strlen(op);
+    len = my_strlen(av[2]);
     for (int i = 0; i < len; i++) {
-        found = validate_op(op[i], n1, n2);
+        found = validate_op(av[2], n1, n2);
         if (found != -1)
             return OPERATORS_FUNCS[found].exec(n1, n2);
     }
