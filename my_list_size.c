6d4c9aa862b37370b7ad68e926727a5353a06a7e
Leticia <leticia.fraga@epitech.eu>
2023-10-16 16:08:58 +0200

    fix?

--- my_list_size.c
+++ my_list_size.c
@@ -12,6 +12,8 @@ int my_list_size(linked_list_t const *begin)
     int n = 1;
     linked_list_t *cur = begin;
 
+    if (cur == 0)
+        return 0;
     while (cur->next != 0) {
         n++;
         cur = cur->next;
