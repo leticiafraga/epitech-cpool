4cef0d5c638c4399161e25ee4a190d19dc421327
Leticia <leticia.fraga@epitech.eu>
2023-10-16 11:58:23 +0200

    task6

--- my_find_node.c
+++ my_find_node.c
@@ -1,21 +1,21 @@
 /*
 ** EPITECH PROJECT, 2023
-** MY_APPLY_ON_MATCHING_NODES
+** MY_APPLY_ON_NODES
 ** File description:
-** Applies f on matching nodes of list
+** Applies f on nodes of list
 */
 
 #include "include/mylist.h"
 
-int my_apply_on_matching_nodes(linked_list_t *begin, int (*f) (),
-    void const *data_ref, int (*cmp)())
+linked_list_t *my_find_node(linked_list_t const *begin,
+    void const * data_ref, int (*cmp)())
 {
     linked_list_t *cur = begin;
 
     while (cur != 0) {
         if (cmp(cur->data, data_ref) == 0)
-            f(cur->data);
+            return cur;
         cur = cur->next;
     }
-    return 0;
+    return cur;
 }
