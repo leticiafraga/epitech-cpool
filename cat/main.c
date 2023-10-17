dddd7606a78e64c0ba76fa24ebd1caf65caa80be
Leticia <leticia.fraga@epitech.eu>
2023-10-17 11:34:29 +0200

    separate files

--- cat/main.c
+++ cat/main.c
@@ -9,29 +9,6 @@
 #include <unistd.h>
 #include "../include/my.h"
 
-void read_files(int ac, char **av)
-{
-    int arrsize = 30000;
-    char *arr[arrsize + 1];
-    int fd = 0;
-    int size = 0;
-
-    for (int i = 1; i < ac; i++) {
-        fd = open(av[i], O_RDONLY);
-        if (fd == -1) {
-            my_put_err("cat: ");
-            my_put_err(av[i]);
-            my_put_err(": No such file or directory\n");
-        }
-        size = read(fd, arr, arrsize);
-        while (size > 0) {
-            write(1, arr, size);
-            size = read(fd, arr, arrsize);
-        }
-        close(fd);
-    }
-}
-
 int main(int ac, char **av)
 {
     int arrsize = 30000;
