97a6307d3619e73e21c73ea1bcced77423da9e15
Leticia <leticia.fraga@epitech.eu>
2023-10-17 10:25:34 +0200

    first try

---
+++ cat/main.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "../include/my.h"
+
+int main(int ac, char **av)
+{
+    char *arr[1000];
+    int fd = 0;
+    int size = 0;
+    
+    if (ac == 0)
+        return 0;
+    for (int i = 1; i < ac; i++) {
+        fd = open(av[i], O_RDONLY);
+        if (fd == -1) {
+            my_put_err("cat: ");
+            my_put_err(av[i]);
+            my_put_err(": No such file or directory\n");
+        }
+        while (size = read(fd, arr, 30) > 0)
+            write(1, arr, 30);
+        close(fd);
+    }
+    return 0;
+}
