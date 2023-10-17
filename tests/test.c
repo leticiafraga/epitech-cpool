0bfd11515310490af2ab9c002501c7189064cb25
Leticia <leticia.fraga@epitech.eu>
2023-10-17 14:08:55 +0200

    initial tests

--- tests/test.c
+++ tests/test.c
@@ -23,3 +23,11 @@ Test(cat, no_file, .init=redirect_all_stdout)
     read_files(2, arr);
     cr_assert_stderr_eq_str("cat: test: No such file or directory\n","");
 }
+
+Test(cat, file, .init=redirect_all_stdout)
+{
+    char *arr[2];
+    arr[1] = "../tests/read_test";
+    read_files(2, arr);
+    cr_assert_stdout_eq_str("test\n","");
+}
