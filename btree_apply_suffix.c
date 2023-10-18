e4781fa44fb054543de4283b8c6c95464c0d115f
Leticia <leticia.fraga@epitech.eu>
2023-10-18 16:40:26 +0200

    fix

--- btree_apply_suffix.c
+++ btree_apply_suffix.c
@@ -9,9 +9,9 @@
 void btree_apply_suffix(btree_t *root, int (*applyf)(void *))
 {
     if (root->left != 0)
-        btree_apply_prefix(root->left, applyf);
+        btree_apply_suffix(root->left, applyf);
     if (root->right != 0)
-        btree_apply_prefix(root->right, applyf);
+        btree_apply_suffix(root->right, applyf);
     if (root->item != 0)
         applyf(root->item);
 }
