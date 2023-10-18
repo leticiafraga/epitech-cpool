93a81a2254a7cf53fe7f3fe674d1c667def26dc7
Leticia <leticia.fraga@epitech.eu>
2023-10-18 15:19:44 +0200

    refactor

---
+++ grep/helpers.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** GREP
+** File description:
+** Clone grep
+*/
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "../include/my.h"
+#include "../include/grep.h"
+
+int get_size(char *arr, int init)
+{
+    int cnt = init;
+
+    while (arr[cnt] != '\n' && arr[cnt] != '\0') {
+        cnt++;
+    }
+    return cnt;
+}
+
+char *get_lines(char *arr, int init, int cnt)
+{
+    int j = 0;
+    char *line = malloc(sizeof(char) * (cnt - init));
+
+    while (init < cnt) {
+	line[j] = arr[init];
+        init++;
+        j++;
+    }
+    line[j] = '\0';
+    return line;
+}
--- include/grep.h
+++ include/grep.h
@@ -1,12 +1,14 @@
 /*
 ** EPITECH PROJECT, 2023
-** CAT
+** GREP
 ** File description:
 ** Functions for cat task
 */
 
-#ifndef CAT_H
-    #define CAT_H
+#ifndef GREP_H
+    #define GREP_H
 void read_files(int ac, char **av);
 void read_input(char *param);
+int get_size(char *arr, int init);
+char *get_lines(char *arr, int init, int cnt);
 #endif
