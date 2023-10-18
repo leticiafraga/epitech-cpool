e7c83f1296801539d86b0405a347771edaf83959
Leticia <leticia.fraga@epitech.eu>
2023-10-18 16:55:37 +0200

    style

--- btree_apply_infix.c
+++ btree_apply_infix.c
@@ -10,8 +10,8 @@ void btree_apply_infix(btree_t *root, int (*applyf)(void *))
 {
     if (root->left != 0)
         btree_apply_infix(root->left, applyf);
-            if (root->item != 0)
-                applyf(root->item);
+    if (root->item != 0)
+        applyf(root->item);
     if (root->right != 0)
         btree_apply_infix(root->right, applyf);
 }
