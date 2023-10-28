73ff7c6fa15fe8b4509984fff7c5ccb38fb08f5a
Leticia <leticia.fraga@epitech.eu>
2023-10-28 13:25:46 +0200

    more tests

--- tests/test.c
+++ tests/test.c
@@ -13,3 +13,21 @@ Test (rush3, rush_1_1_rec_3_3, .init=cr_redirect_stdout)
     rush3("o-o\n| |\no-o\n");
     cr_assert_stdout_eq_str("[rush-1-1] 3 3\n");
 }
+
+Test (rush3, rush_1_1_rec_1_1, .init=cr_redirect_stdout)
+{
+    rush3("o\n");
+    cr_assert_stdout_eq_str("[rush-1-1] 1 1\n");
+}
+
+Test (rush3, test_1_3, .init=cr_redirect_stdout)
+{
+    rush3("o\n|\no\n");
+    cr_assert_stdout_eq_str("[rush-1-1] 1 3\n");
+}
+
+Test (rush3, test_3_1, .init=cr_redirect_stdout)
+{
+    rush3("o-o\n");
+    cr_assert_stdout_eq_str("[rush-1-1] 3 1\n");
+}
