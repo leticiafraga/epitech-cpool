44b66f06d4f296f55375cb5fcfa7b18ebb5fc70a
Leticia <leticia.fraga@epitech.eu>
2023-10-28 14:45:06 +0200

    buff size header

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <unistd.h>
 #include "include/rush3.h"
+#include "include/buff_size.h"
 int main()
 {
     char buff[BUFF_SIZE + 1];
     int offset = 0;
     int len ;
 
-    while ((len = read(0 , buff + offset , BUFF_SIZE - offset)) > 0) {
-    offset = offset + len ;
+    while ((len = read(0, buff + offset, BUFF_SIZE - offset)) > 0) {
+        offset = offset + len;
     }
     buff[offset] = '\0';
     if (len < 0)
         return (84) ;
-    rush3(buff) ;
-    return (0) ;
+    rush3(buff);
+    return (0);
 }
