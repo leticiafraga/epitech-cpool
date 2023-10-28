1a38faa438b88cd3b9779a52c446376f214a3f7a
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2023-10-28 17:50:20 +0200

    bonus, error messages

---
+++ Bonus/rectangle1.c
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2023
+** Stumper
+** File description:
+** Define what is the rush function called
+*/
+#include <stdlib.h>
+#include "include/my.h"
+#include "include/struct.h"
+#include "include/rush3.h"
+
+void put_dimensions(int w, int h)
+{
+    my_put_nbr(w);
+    my_putchar(' ');
+    my_put_nbr(h);
+    my_putchar('\n');
+}
+
+int not_found(char *str)
+{
+    my_put_err(str);
+    my_putstr("none\n");
+    return -1;
+}
+
+rectangle *init_rec_1(void)
+{
+    rectangle *rec = malloc(sizeof(rectangle));
+
+    rec->tl_corner = 'o';
+    rec->tr_corner = 'o';
+    rec->bl_corner = 'o';
+    rec->br_corner = 'o';
+    rec->h_body = '-';
+    rec->v_body = '|';
+    return rec;
+}
+
+int rectangle1(char *buffer)
+{
+    int width = 0;
+    int height = 0;
+    rectangle *rec;
+
+    rec = init_rec_1();
+    width = check_first_line(buffer, rec->h_body, rec->tr_corner, 1);
+    if (width < 0)
+        return not_found("Error in the first line\n");
+    height = check_vertical(buffer, width, rec->v_body);
+    if (height < 0)
+        return not_found("Error in the vertical lines\n");
+    buffer += (width + 1) * (height - 1);
+    if (check_last_line(buffer, rec, width, 1) < 0)
+        return not_found("Error in the last line\n");
+    my_putstr("[rush-1-1] ");
+    put_dimensions(width, height);
+}
